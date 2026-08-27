2cd1eb55ef6f6b048aedb621b971fad7721029ac
Calin Alexandru Cosmin <calinalexandrucosmin89@gmail.com>
2026-08-27 20:29:41 +0300

    Added CI with github actions

--- backend/src/main/java/com/loocos/Main.java
+++ backend/src/main/java/com/loocos/Main.java
@@ -19,6 +19,8 @@ public class Main {
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository) {
 
+
+
         var faker = new Faker();
         Random random = new Random();
         var name = faker.name();

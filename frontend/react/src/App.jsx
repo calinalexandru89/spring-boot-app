bb9d68751c06f7aa6a2b17afb71bf5bee87de2ad
Calin Alexandru Cosmin <calinalexandrucosmin89@gmail.com>
2026-09-01 19:38:08 +0300

    frontend started with react + JavaScript + Chackra UI

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+const  App = () => {
+    return (
+        <Button colorScheme='teal' variant='outline'>
+            Click me
+        </Button>
+    )
+}
+
+export default  App;

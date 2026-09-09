bf48f9cedbecd65658830ed373eb83ee2a46da0b
Calin Alexandru Cosmin <calinalexandrucosmin89@gmail.com>
2026-09-09 19:57:09 +0300

    Added CD for frontend v5

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -107,7 +107,7 @@ const NavItem = ({icon, route, children, ...rest}) => {
                 role="group"
                 cursor="pointer"
                 _hover={{
-                    bg: 'green.400',
+                    bg: 'red.400',
                     color: 'white',
                 }}
                 {...rest}>
